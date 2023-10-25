d3205eb2fcc1e37036d6661ba91ef94c08b333f5
everduin94 <everduin94@gmail.com>
2023-10-25 11:05:44 -0500

    🔨 refactor(example): todo list
    small fixes to the todo list component (nested signals)

--- src/app/fine-grain/fine-grain.component.ts
+++ src/app/fine-grain/fine-grain.component.ts
@@ -1,37 +1,39 @@
 import { Component, WritableSignal, computed, signal } from '@angular/core';
 
 interface Todo {
-  title: string,
-  complete: WritableSignal<boolean>,
+  title: string;
+  complete: WritableSignal<boolean>;
 }
 
 @Component({
   selector: 'app-fine-grain',
   templateUrl: './fine-grain.component.html',
-  styleUrls: ['./fine-grain.component.css']
+  styleUrls: ['./fine-grain.component.css'],
 })
 export class FineGrainComponent {
-
-
   hideCompleted = signal(false);
 
   todoList = signal<Todo[]>([
-    {title: 'Feed puppies', complete: signal(true)},
-    {title: 'Burp baby', complete: signal(false)},
-    {title: 'Clean cat litter', complete: signal(false)},
-  ])
+    { title: 'Refactor entire app', complete: signal(true) },
+    { title: '????', complete: signal(false) },
+    { title: 'Profit!!!', complete: signal(false) },
+  ]);
 
-  filteredList = computed(() => !this.hideCompleted() ? this.todoList() : this.todoList().filter(t => !t.complete()))
+  filteredList = computed(() =>
+    !this.hideCompleted()
+      ? this.todoList()
+      : this.todoList().filter((t) => !t.complete())
+  );
 
   updateTodo(complete: WritableSignal<boolean>) {
-    complete.update(c => !c)
+    complete.update((c) => !c);
   }
 
   toggleCompleted() {
-    this.hideCompleted.update(c => !c)
+    this.hideCompleted.update((c) => !c);
   }
 
-  addTodo(title: any) {
-    this.todoList.update(v => [...v, {title, complete: signal(false)}])
+  addTodo(title: string) {
+    this.todoList.update((v) => [...v, { title, complete: signal(false) }]);
   }
 }
